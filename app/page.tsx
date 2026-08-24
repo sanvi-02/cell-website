19e67fabf5bd20d4c55158b32ff34a4c406022f1
Vedanshu <vedanshumeharia0@gmail.com>
2026-08-24 11:22:12 +0530

    complete hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
+import Intro from "@/components/Intro";
+import Features from "@/components/Feature";
 
 export default function Home(){
   return(
     <main>
         <Navbar />
         <Hero />
+        <Intro/>
+        <Features/>
     </main>
   )
 }
